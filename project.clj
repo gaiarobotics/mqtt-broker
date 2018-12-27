04cd3346602937027bf4c646a9bf8f11f8cba154
nex <andresgu@gmail.com>
2018-12-27 00:16:43 -0600

    copies everything (#) to mongo

--- project.clj
+++ project.clj
@@ -3,4 +3,14 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :main mqtt-broker.core
+  :repl-options {
+    :init-ns mqtt-broker.core
+    :init (println "entrando a " *ns*)
+    :prompt (fn [ns] (str ns "-> "))
+    :welcome (println "Hols")
+  }
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.json "0.2.6"]
+                 [clojurewerkz/machine_head "1.0.0"]
+                 [mongerr "1.0.0-SNAPSHOT"]])
--- src/mqtt_broker/core.clj
+++ src/mqtt_broker/core.clj
@@ -1,6 +1,29 @@
-(ns mqtt-broker.core)
+(ns mqtt-broker.core
+  (:gen-class)
+  (:require [clojure.data.json :as json]
+            [clojurewerkz.machine-head.client :as mh]
+            [mongerr.core :as db]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn parsed-payload [payload]
+  (let [s (String. payload "UTF-8")
+        js (try (json/read-str s) (catch Exception e))]
+    (if (map? js) js s)))
+
+(defn -main
+  [& args]
+  (let [conn (mh/connect "tcp://127.0.0.1:1883")]
+    (mh/subscribe conn {"#" 0} (fn [^String topic _ ^bytes payload]
+                                     (println (String. payload "UTF-8"))
+                                     (println topic)
+                                     (db/db-insert topic (parsed-payload payload))))))
+
+; (mh/publish conn "topico/a/" "Hells, worldo")
+
+(defn test
+  [& args]
+  (let [conn (mh/connect "tcp://127.0.0.1:1883")]
+    (mh/subscribe conn {"hello" 0} (fn [^String topic _ ^bytes payload]
+                                   (println (String. payload "UTF-8"))
+                                   (mh/disconnect conn)
+                                   (System/exit 0)))
+    (mh/publish conn "hello" "Hello, world")))
