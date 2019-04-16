99d5471685eb1adc5cc73efb4b761e8aeee80c49
nex <andresgu@gmail.com>
2019-04-16 12:58:31 -0500

    exception handling in -main

--- src/mqtt_broker/core.clj
+++ src/mqtt_broker/core.clj
@@ -4,6 +4,9 @@
             [clojurewerkz.machine-head.client :as mh]
             [mongerr.core :as db]))
 
+(def url "tcp://127.0.0.1:1883")
+(def conn (mh/connect url))
+
 (defn parsed-payload [payload]
   (let [s (String. payload "UTF-8")
         js (try (json/read-str s) (catch Exception e))]
@@ -11,19 +14,30 @@
 
 (defn -main
   [& args]
-  (let [conn (mh/connect "tcp://127.0.0.1:1883")]
-    (mh/subscribe conn {"#" 0} (fn [^String topic _ ^bytes payload]
-                                     (println (String. payload "UTF-8"))
-                                     (println topic)
-                                     (db/db-insert topic (parsed-payload payload))))))
+  (mh/subscribe conn {"#" 0} (fn [^String topic _ ^bytes payload]
+                               (try
+                                 (println "topic: " topic)
+                                 (println "payload: " (parsed-payload payload))
+                                 (db/db-insert topic (parsed-payload payload))
+                               (catch Exception e (println e))))))
 
-; (mh/publish conn "topico/a/" "Hells, worldo")
+(defn db-get []
+  (mh/subscribe (mh/connect url) ; conn
+    {"db-get" 0} (fn [^String topic _ ^bytes payload]
+                   (try
+		     (let [payload    (parsed-payload payload)
+                           collection (:collection payload)
+                           query      (dissoc payload :collection)
+                           result     (db/db collection query)]
+                       (mh/publish conn "db-get" {:collection collection
+                                                  :query      query
+                                                  :result     result}))
+		     (catch Exception e (println "Exception e: e"))))))
 
 (defn test
   [& args]
-  (let [conn (mh/connect "tcp://127.0.0.1:1883")]
-    (mh/subscribe conn {"hello" 0} (fn [^String topic _ ^bytes payload]
+  (mh/subscribe conn {"hello" 0} (fn [^String topic _ ^bytes payload]
                                    (println (String. payload "UTF-8"))
                                    (mh/disconnect conn)
                                    (System/exit 0)))
-    (mh/publish conn "hello" "Hello, world")))
+  (mh/publish conn "hello" "Hello, world"))
